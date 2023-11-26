3dcb179ebd9bad1ed036037d1b9ca84fc8874e8c
Usman <usman.solemn@gmail.com>
2023-11-26 20:54:25 +0500

    Rabbitmq fanout communication Producer to Consumer

---
+++ Messaging_Brokers/RabbitMq_Code/Consoles/ConsoleApp.Consumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace ConsoleApp.Consumer
+{
+    internal class Program
+    {
+        private static string AMQP_URL = "";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri(AMQP_URL)
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "ulogs", type: ExchangeType.Fanout);
+
+            // declare a server-named queue
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName,
+                              exchange: "ulogs",
+                              routingKey: string.Empty);
+
+            Console.WriteLine(" [*] Waiting for logs.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                byte[] body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] {message}");
+            };
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Messaging_Brokers/RabbitMq_Code/Consoles/ConsoleApp.Producer/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace ConsoleApp.Producer
+{
+    internal class Program
+    {
+        private static string AMQP_URL = "";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri(AMQP_URL)
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "ulogs", type: ExchangeType.Fanout);
+
+            while (true)
+            {
+                Console.WriteLine("Type your message to send!, press ESC to exit");
+
+                var message = Console.ReadLine();
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "ulogs",
+                                     routingKey: string.Empty,
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine($" [x] Sent {message}");
+
+
+            }
+
+
+        }
+
+
+    }
+}
